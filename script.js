2f6934462b4293628ed09cf8a5acf99d436201eb
Adam-T0th <adam.toth@webtrends-optimize.com>
2023-01-17 15:11:32 +0000

    refactored

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 /*jshint esversion: 6 */
+// helper function for event delegation
+var delegate = (function(){
+    if (!Element.prototype.matches) {
+        Element.prototype.matches = Element.prototype.msMatchesSelector ||
+            Element.prototype.webkitMatchesSelector;
+    }
+    return function(el, evt, sel, handler) {
+        el.addEventListener(evt, function (event) {
+            var t = event.target;
+            while (t && t !== this) {
+                if (t.matches(sel)) {
+                    handler.call(t, event);
+                }
+                t = t.parentNode;
+            }
+        });
+    };
+}());
+
+/* global variables */
+let timeObtained = false;
 let today,
     time,
     hoursString,
@@ -30,7 +51,8 @@ const getDate = () => {
 
 const refreshDate =  setInterval(() => {
     getDate();
-    document.getElementById('stopTime').addEventListener('click', function() {
+
+    delegate(document, 'click', '#stopTime', function() {
         clearInterval(refreshDate);
         console.log('>>> stopped counter');
     });
@@ -38,101 +60,109 @@ const refreshDate =  setInterval(() => {
 
 
 let hoursColumn_1 = document.querySelectorAll('div#hour-column-1 div');
+let hoursColumn_2 = document.querySelectorAll('div#hour-column-2 div');
+let minutesColumn_1 = document.querySelectorAll('div#minute-column-1 div');
+let minutesColumn_2 = document.querySelectorAll('div#minute-column-2 div');
+let secondsColumn_1 = document.querySelectorAll('div#seconds-column-1 div');
+let secondsColumn_2 = document.querySelectorAll('div#seconds-column-2 div');
+
 let update_H1 = () => {
-    for(let i = 0; i < hoursColumn_1.length; i++) {
-        if((hoursColumn_1[i].classList[0].match(/\d/)[0].length)) {
-            if(parseInt(hoursColumn_1[i].classList[0].match(/\d/)[0]) == (`${hoursString}`).split('')[0]) {
-                hoursColumn_1[i].innerHTML = (`${hoursString}`).split('')[0];
+    hoursColumn_1.forEach(function(entry, index) {
+        if(entry.classList[0].match(/\d/)[0].length) {
+            if(parseInt(entry.classList[0].match(/\d/)[0]) === (`${hoursString}`).split('')[0]) {
+                entry.innerHTML = (`${hoursString}`).split('')[0];
             }
         } else {
             console.log('>>> no match found for hours(1)');
         }
-    }
+    });
 };
 
-let hoursColumn_2 = document.querySelectorAll('div#hour-column-2 div');
 let update_H2 = () => {
-    for(let i = 0; i < hoursColumn_2.length; i++) {
-        if((hoursColumn_2[i].classList[0].match(/\d/)[0].length)) {
-            if(parseInt(hoursColumn_2[i].classList[0].match(/\d/)[0]) == (`${hoursString}`).split('')[0]) {
-                hoursColumn_2[i].innerHTML = (`${hoursString}`).split('')[1];
+    hoursColumn_1.forEach(function(entry, index) {
+        if(entry.classList[0].match(/\d/)[0].length) {
+            if(parseInt(entry.classList[0].match(/\d/)[0]) === (`${hoursString}`).split('')[0]) {
+                entry.innerHTML = (`${hoursString}`).split('')[1];
             }
         } else {
             console.log('>>> no match found for hours(2)');
         }
-    }
+    });
 };
 
-let minutesColumn_1 = document.querySelectorAll('div#minute-column-1 div');
 let update_M1 = ()  => {
-    for(let i = 0; i < minutesColumn_1.length; i++) {
-        if((minutesColumn_1[i].classList[0].match(/\d/)[0].length)) {
-            if(parseInt(minutesColumn_1[i].classList[0].match(/\d/)[0]) == (`${minutesString}`).split('')[0]) {
-                minutesColumn_1[i].innerHTML = (`${minutesString}`).split('')[0];
+    minutesColumn_1.forEach(function(entry, index) {
+        if(entry.classList[0].match(/\d/)[0].length) {
+            if(parseInt(entry.classList[0].match(/\d/)[0]) === (`${minutesString}`).split('')[0]) {
+                entry.innerHTML = (`${minutesString}`).split('')[0];
             }
         } else {
             console.log('>>> no match found for minutes(1)');
         }
-    }
+    });
 };
 
-let minutesColumn_2 = document.querySelectorAll('div#minute-column-2 div');
 let update_M2 = () => {
-    for(let i = 0; i < minutesColumn_2.length; i++) {
-        if((minutesColumn_2[i].classList[0].match(/\d/)[0].length)) {
-            if(parseInt(minutesColumn_2[i].classList[0].match(/\d/)[0]) == (`${minutesString}`).split('')[1]) {
-                minutesColumn_2[i].innerHTML = (`${minutesString}`).split('')[1];
+    minutesColumn_2.forEach(function(entry, index) {
+        if(entry.classList[0].match(/\d/)[0].length) {
+            if(parseInt(entry.classList[0].match(/\d/)[0]) === (`${minutesString}`).split('')[1]) {
+                entry.innerHTML = (`${minutesString}`).split('')[1];
             }
         } else {
             console.log('>>> no match found for minutes(2)');
         }
-    }
+    });
 };
 
-let secondsColumn_1 = document.querySelectorAll('div#seconds-column-1 div');
 let update_S1 = () => {
-    for(let i = 0; i < secondsColumn_1.length; i++) {
-        if((secondsColumn_1[i].classList[0].match(/\d/)[0].length)) {
-            if(parseInt(secondsColumn_1[i].classList[0].match(/\d/)[0]) == (`${secondsString}`).split('')[0]) {
-                secondsColumn_1[i].innerHTML = (`${secondsString}`).split('')[0];
+    secondsColumn_1.forEach(function(entry, index) {
+        if(entry.classList[0].match(/\d/)[0].length) {
+            if(parseInt(entry.classList[0].match(/\d/)[0]) === (`${secondsString}`).split('')[0]) {
+                entry.innerHTML = (`${secondsString}`).split('')[0];
             }
         } else {
             console.log('>>> no match found for seconds(1)');
         }
-    }
+    });
 };
 
-let secondsColumn_2 = document.querySelectorAll('div#seconds-column-2 div');
 let update_S2 = () => {
-    for(let i = 0; i < secondsColumn_2.length; i++) {
-        if((secondsColumn_2[i].classList[0].match(/\d/)[0].length)) {
-            if(parseInt(secondsColumn_2[i].classList[0].match(/\d/)[0]) == (`${secondsString}`).split('')[1]) {
-                secondsColumn_2[i].innerHTML = (`${secondsString}`).split('')[1];
+    // reset previous cell
+    // if(entry[index - 1].classList[0].match(/\d/)[0].length) {
+    //     entry[index - 1].innerHTML = '';
+    // }
+
+    secondsColumn_2.forEach(function(entry, index) {
+        if(entry.classList[0].match(/\d/)[0].length) {
+            if(parseInt(entry.classList[0].match(/\d/)[0]) === (`${secondsString}`).split('')[1]) {
+                entry.innerHTML = (`${secondsString}`).split('')[1];
             }
         } else {
-            console.log('>>> no match found for seconds(1)');
+            console.log('>>> no match found for seconds(2)');
         }
-    }
+    });
 };
 
-let timeObtained = false;
 let timePromise = new Promise((resolve, reject) => {
     resolve('>>> time obtained');
     timeObtained = true;
+
+    reject('>>> error in Promise');
 });
 
-
+// if time is obtained, update cells with corresponding values
 timePromise.then(() => {
     if(timeObtained) {
         console.log('>>> timeObtained is true');
         console.log('>>> call functions and start re-evaluating');
-        setInterval(() => {
+        let updateCells = setInterval(() => {
             update_H1();
             update_H2();
             update_M1();
             update_M2();
             update_S1();
             update_S2();
+            delegate(document, 'click', '#stopTime', function() { clearInterval(updateCells); });
         }, 1000);
     }
 });
